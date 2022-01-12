1dcc8c55beb845e4215fd237e1ce3e810de5d013
Marek Männiksaar <m.manniksaar@gmail.com>
2022-01-12 19:39:46 +0200

    commit

--- store/views.py
+++ store/views.py
@@ -4,6 +4,7 @@ from category.models import Category
 from carts.models import CartItem
 from carts.views import _cart_id
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.db.models import Q
 from django.shortcuts import HttpResponse
 
 # Create your views here.
@@ -50,8 +51,11 @@ def search(request):
     if 'keyword' in request.GET:
         keyword = request.GET['keyword']
         if 'keyword':
-            products = Product.objects.order_by('-created_date').filter(description__icontains=keyword)
+            products = Product.objects.order_by('-created_date').filter(Q(description__icontains=keyword) | Q(product_name__icontains=keyword)) # Q using in complex querys
+            product_count = products.count()
+
     context = {
         'products' : products,
+        'product_count': product_count,
     }
     return render(request, 'store/store.html', context)
