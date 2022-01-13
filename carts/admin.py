8a4f456480d8c1787dd35808ebced0dcc4d59fd3
Marek Männiksaar <m.manniksaar@gmail.com>
2022-01-13 18:22:55 +0200

    connit

--- carts/admin.py
+++ carts/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from store.models import Product
+
 from carts.models import Cart, CartItem
 
 # Register your models here.
-admin.site.register(Cart)
-admin.site.register(CartItem)
+
+class CartAdmin(admin.ModelAdmin):
+    list_display = ('cart_id', 'add_date')
+
+class CartItemAdmin(admin.ModelAdmin):
+
+    list_display = ('product', 'cart', 'quantity', 'is_active',)
+
+admin.site.register(Cart, CartAdmin)
+admin.site.register(CartItem, CartItemAdmin)
