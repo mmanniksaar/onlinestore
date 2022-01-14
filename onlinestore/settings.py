b7856a0e19af9ce262016ecf2e82389eabffc326
Marek Männiksaar <m.manniksaar@gmail.com>
2022-01-14 20:52:26 +0200

    commit

--- onlinestore/settings.py
+++ onlinestore/settings.py
@@ -98,9 +98,7 @@ DATABASES = {
     }
 }
 
-# import dj_database_url
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -151,3 +149,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
+
+
+
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
