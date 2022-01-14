2a1b205936ab4d09474ee09414ba8eba8d530d7c
Marek Männiksaar <m.manniksaar@gmail.com>
2022-01-14 20:55:40 +0200

    commit

--- onlinestore/settings.py
+++ onlinestore/settings.py
@@ -91,12 +91,12 @@ WSGI_APPLICATION = 'onlinestore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': str(BASE_DIR / 'db.sqlite3'),
+#     }
+# }
 
 
 
